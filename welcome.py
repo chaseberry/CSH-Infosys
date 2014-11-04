90b8bd3a68fabd52fc632751d063e98272fe27a6
chase <chase@bryx.com>
2014-11-03 20:22:41 -0500

    Made this a thing

---
+++ welcome.py
@@ -0,0 +1,12 @@
+from infosys import infosys
+
+infosysKey = '2b62ff22-a815-42e3-9e71-c8480ffbf3fd'
+
+def setup():
+    global infosysKey
+    info = infosys(infosysKey)
+    info.addText(0, 'TO CSH! <PICTUREFILE:1>', 'WELCOME')
+    info.addPicture(1, ['2222226', '2111026', '2100626', '2111666', '2001626', '2111026', '2222226'])
+
+if __name__ == '__main__':
+    setup()
